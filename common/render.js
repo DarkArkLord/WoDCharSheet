601d4ecef64f27b63fdccd80385d95a41d10dfae
DarkLord <dfhrhfan12@yandex.ru>
2025-01-09 19:29:16 +0400

    Add render lib

---
+++ common/render.js
@@ -0,0 +1,43 @@
+const HTMLTags = {
+    Div: 'div',
+    Table: 'table',
+    TableRow: 'tr',
+    TableData: 'td',
+    Img: 'img',
+    h2: 'h2',
+    TextArea: 'textarea',
+    Button: 'button',
+};
+
+function render(tag, attributes, ...childs) {
+    if (tag instanceof Function) {
+        return tag(attributes, ...childs);
+    }
+
+    const element = document.createElement(tag);
+
+    if (attributes) {
+        for (const name in attributes) {
+            let value = attributes[name];
+            element.setAttribute(name, value);
+        }
+    }
+
+    for (const child of childs) {
+        (function addChild(parent, child) {
+            if (Array.isArray(child)) {
+                for (const innerChild of child) {
+                    addChild(parent, innerChild);
+                }
+            } else {
+                parent.appendChild(
+                    typeof child == 'number' || typeof child == 'string'
+                        ? document.createTextNode(child)
+                        : child
+                );
+            }
+        })(element, child);
+    }
+
+    return element;
+}

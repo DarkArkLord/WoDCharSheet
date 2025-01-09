fdded8c8728f9537d20243ca0de1f3a8d07beba2
DarkLord <dfhrhfan12@yandex.ru>
2025-01-09 13:26:59 +0400

    Upd styles

--- common/tabs.js
+++ common/tabs.js
@@ -2,21 +2,18 @@ function configureTabsAndButtons(input) {
     const {
         tabs,
         activeTabIndex,
-        styles: {
-            ACTIVE_BUTTON_CLASS,
-            DISPLAY_BLOCK_CLASS,
-        },
+        activeStyleClass,
     } = input;
 
     for (const curTab of tabs) {
         curTab.button.onclick = function () {
             for (const tab of tabs) {
-                tab.button.classList.remove(ACTIVE_BUTTON_CLASS);
-                tab.content.classList.remove(DISPLAY_BLOCK_CLASS);
+                tab.button.classList.remove(activeStyleClass);
+                tab.content.classList.remove(activeStyleClass);
             }
 
-            curTab.button.classList.add(ACTIVE_BUTTON_CLASS);
-            curTab.content.classList.add(DISPLAY_BLOCK_CLASS);
+            curTab.button.classList.add(activeStyleClass);
+            curTab.content.classList.add(activeStyleClass);
         };
     }
 

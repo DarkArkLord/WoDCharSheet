c87e539d89c2a1d188e83c4bd6d98757288fef4a
DarkLord <dfhrhfan12@yandex.ru>
2025-01-10 14:31:05 +0400

    Add states

--- src/setting/MtA20.js
+++ src/setting/MtA20.js
@@ -1,9 +1,35 @@
-const charConfig = Object.freeze({
+export const charConfig = Object.freeze({
     states: {
-        title: 'attributes',
+        title: 'Attributes',
         translation: 'Атрибуты',
         sections: [
-            //
+            {
+                title: 'Physical',
+                translation: 'Физические',
+                values: [
+                    { title: 'Strength', translation: 'Сила', },
+                    { title: 'Dexterity', translation: 'Ловкость', },
+                    { title: 'Stamina', translation: 'Выносливость', },
+                ]
+            },
+            {
+                title: 'Social',
+                translation: 'Социальные',
+                values: [
+                    { title: 'Charisma', translation: 'Харизма', },
+                    { title: 'Manipulation', translation: 'Манипуляция', },
+                    { title: 'Appearance', translation: 'Внешность', },
+                ]
+            },
+            {
+                title: 'Mental',
+                translation: 'Ментальные',
+                values: [
+                    { title: 'Perception', translation: 'Восприятие', },
+                    { title: 'Intelligence', translation: 'Интеллект', },
+                    { title: 'Wits', translation: 'Смекалка', },
+                ]
+            },
         ],
     },
 });

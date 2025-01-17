47ffa7a3302577908af474e2d34b0cf67024c30d
DarkLord <dfhrhfan12@yandex.ru>
2025-01-18 00:34:31 +0400

    Add disabling to readonly

--- src/common/uiElements.js
+++ src/common/uiElements.js
@@ -38,6 +38,7 @@ export class UITextInput {
 
     setReadOnly(isReadOnly) {
         this.input.readOnly = isReadOnly;
+        this.input.disabled = isReadOnly;
     }
 
     setVisible(isVisible) {

e3640b8f025e015948bb7962a32e3844db274d1f
DarkLord <dfhrhfan12@yandex.ru>
2025-02-01 21:09:19 +0400

    === Add TS ===

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,14 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const environment = require('./configuration/environment');
 
+const SYSTEMS = Object.freeze({
+    MageTheAscension20: 'MtA20',
+});
+
 module.exports = {
     target: 'web',
     entry: {
-        MtA20: path.resolve(environment.paths.source, 'entry', 'MtA20.js'),
+        [SYSTEMS.MageTheAscension20]: path.resolve(environment.paths.source, 'entry', `${SYSTEMS.MageTheAscension20}.js`),
     },
     output: {
         filename: '[name].js',
@@ -15,6 +19,10 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /\.ts$/i,
+                loader: 'ts-loader',
+            },
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
@@ -33,9 +41,9 @@ module.exports = {
         }),
         new HTMLWebpackPlugin({
             template: path.resolve(environment.paths.source, 'blank.html'),
-            filename: 'MtA20.html',
+            filename: `${SYSTEMS.MageTheAscension20}.html`,
             inject: true,
-            chunks: 'MtA20.js',
+            chunks: `${SYSTEMS.MageTheAscension20}.js`,
         }),
     ],
 };
